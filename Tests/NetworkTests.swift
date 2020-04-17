8cf2e7e8813b0ec55a21ab5afa11b736e9abb7ee
Serge Maslyakov <serge.maslyakov@gmail.com>
2020-04-17 18:48:08 +0400

    - fixed test target

--- Tests/NetworkTests.swift
+++ Tests/NetworkTests.swift
@@ -1,20 +1,25 @@
 import XCTest
-@testable import NetworkFramework
 
 class NetworkTests: XCTestCase {
 
-    override func setUp() {
-        super.setUp()
-
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
     }
 
-    override func tearDown() {
-
-        super.tearDown()
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testAll() {
-        // TODO
-        XCTAssert(true)
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
 }
